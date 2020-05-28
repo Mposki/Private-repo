441821c3d114650743674e61e0d4e944e9df7364
Mposki <mposki125@gmail.com>
2020-05-28 19:07:15 +0200

    Deleted unintresting projects

--- Regex/Regex/Regrex.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-#include <regex>
-using namespace std;
-//ZADANIE 2A
-void PrintMatches(string str, regex reg);
-//ZADANIE 2B
-void PrintMatches2(string str, regex reg);
